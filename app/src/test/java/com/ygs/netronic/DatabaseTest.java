615ecf55f13e289971485e74a56b6f39a178148b
igorsan98v2 <igor.yutsyk@gmail.com>
2020-07-06 20:30:46 +0300

    initial commit:  - selected architecture patterns;  - developed interface for tests;  - selected base tools & libraries for project;

---
+++ app/src/test/java/com/ygs/netronic/DatabaseTest.java
@@ -0,0 +1,30 @@
+package com.ygs.netronic;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DatabaseTest {
+
+
+
+    @Test
+    public void stage0_userWrite(){
+
+    }
+
+    @Test
+    public void stage1_userRead(){
+
+    }
+
+
+    @Test
+    public void stage2_userDelete() {
+
+    }
+
+
+
+}
---
+++ app/src/test/java/com/ygs/netronic/RandomUserApiTest.java
@@ -0,0 +1,15 @@
+package com.ygs.netronic;
+
+import org.junit.Test;
+
+public class RandomUserApiTest {
+    @Test
+    public void getUsers(){
+
+    }
+    
+    @Test
+    public void getWellKnownUser(){
+
+    }
+}
